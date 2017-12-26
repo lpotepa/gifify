62fd73f950190e2ff8744fe3c5390a7f2a1d1efc
Łukasz Potępa <lpotepa@o2.pl>
2017-12-26 01:37:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -153,8 +153,8 @@ function computeConvertArgs(opts) {
       '-fill', 'white',
       '-stroke', 'black',
       '-strokewidth', '1',
-      '-pointsize', '20',
-      '-font', 'Times-Bold',
+      '-pointsize', '30',
+      '-font', 'Courier-Bold',
       '-annotate', '+5+5', opts.text
     );
   }
